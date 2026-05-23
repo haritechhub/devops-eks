2f01c3be2c232863ea104e360106825183decad4
haritechhub <hariharan3429466@gmail.com>
2026-05-23 10:59:32 +0530

    Added all DevOps files

---
+++ terraform/main.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "devops-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Name = "devops-vpc"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.0.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.27"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_groups = {
+    devops_nodes = {
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 2
+      instance_types = [var.instance_type]
+    }
+  }
+
+  tags = {
+    Name = "devops-eks"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "cluster_name" {
+  default = "devops-eks-cluster"
+}
+
+variable "instance_type" {
+  default = "t3.medium"
+}
